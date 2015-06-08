a772ded0a8e3377ce75f9b772c4dc934da904659
Mário Brandão <mariobrandaobr@hotmail.com>
2015-06-08 19:21:32 -0300

    Affix

--- assets/modules/mercurios-js/mercurios.js
+++ assets/modules/mercurios-js/mercurios.js
@@ -12,7 +12,7 @@ jQuery.fn.bgRandom = function(settings){
 
 	if (settings) {
 		$.extend(parametros, settings);
-	}
+	};
 
 	return this.each(function(){
 			
@@ -36,5 +36,44 @@ jQuery.fn.bgRandom = function(settings){
 			'background': "url("+ bg.url +")",
 			'background-repeat': parametros.repetir
 		});
+	});
+};
+
+jQuery.fn.Affix = function(settings){
+	var w = window;
+	var scrollX = pageXOffset;
+	var scrollY = pageYOffset;
+	var $this = $(this);
+	var height = $this.outerHeight(true);
+	var width = $this.outerWidth(true);
+
+	var parametros = {
+		marginTop: "0",
+		marginLeft: "0",
+		marginBottom: "0",
+		marginRight: "0"
+	};
+
+	var p = parametros;
+
+	if (settings) {
+		$.extend(parametros, settings);
+	};
+
+	return this.each(function(){
+		var affix = function() {
+			if (w.scrollY -= w.scrollY - height) {
+				$this.parent().prepend('<div class="sustain"></div>');
+				$(".sustain").css({"height": height,"width": width, "float": "left"});
+				$this.css({
+					"position": "fixed",
+					"z-index": "1000",
+					"margin-left": p.marginLeft,
+					"margin-top": p.marginTop,
+					"margin-right": p.marginRight,
+					"margin-bottom": p.marginBottom
+				});
+			};
+		}();
 	});
 };
